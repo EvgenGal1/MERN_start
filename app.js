10cd37bb702dcf03a7e1166b7854d09566fabd92
EvGen Gal <genwolk1@gmail.com>
2023-01-09 23:52:21 +0300

    fn обёртк. для mongoose и регистр в MongoDB ч/з vpn

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ console.log("App");
 const express = require("express");
 // подкл.ф.наст.конфиг.
 const config = require("config");
+// подкл. mongoDB ч/з mongoose для базы данных
+const mongoose = require("mongoose");
 
 // server,резулт.раб.fn express
 const app = express();
 
 // в конст PORT запис.порт из config или 5000
 const PORT = config.get("port") || 5000;
+
+// fn обёртк. для mongoose
+async function start() {
+  // всё верно
+  try {
+    // await для ожидан. промиса. Вызов mongoose.мтд.connect. 1ый парам url адрес с БД(из config), 2ой парам. набор опций
+    await mongoose.connect(config.get("mongoUri"), {});
+  } catch (error) {
+    // отраб.ошб.
+    console.log("Server Error", error.message);
+    // выход ч/з глоб.проц.мтд .exit
+    process.exit(1);
+  }
+}
 
 // вызов сервера с паредачей порта и fn колбэк (listen - слушатель)
 app.listen(PORT, () =>
