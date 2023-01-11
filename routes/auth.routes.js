9c7742f2c6e8dbfcbe749999a5d67b6ed696d91a
EvGen Gal <genwolk1@gmail.com>
2023-01-11 16:11:51 +0300

    post.register всё. + bcryptjs(шифр пароля), + express-validator(валидация)

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,33 +1,73 @@
 // маршрутизатор аутентификации
 // подкл. Маршрутизатор
 const { Router } = require("express");
-// созд. Маршрутизатор
-const router = Router();
 // подкл.модел.польз.
 const User = require("../models/User");
+// подкл. библ. для шифрование пароля нов.польз.
+const bcrypt = require("bcryptjs");
+// подкл. валидацию
+const { check, validationResult } = require("express-validator");
+// созд. Маршрутизатор
+const router = Router();
 
-// обраб 2 post запроса(inpointer). 1ый парам. конкатенация пути с /api/auth, 2ой fn логики,асинхр,Запрос,Ответ
+// обраб 2 post запроса(inpointer). парам.: str.конкатенация пути с /api/auth, масс.валидации, fn логики(асинхр,Запрос,Ответ)
 // Регистрация. /api/auth/register
-router.post("/register", async (req, res) => {
-  // базов.логика с обраб.ошб.
-  try {
-    // получ.данн.с fronta
-    const { email, password } = req.body;
+router.post(
+  "/register",
+  // масс. middleware для валидации. `Проверка`(чего,ошб.).валидатор(на email) | ~кастом - проверка(чего).условие.смс ошб.
+  [
+    check("email", "Некорректый email").isEmail(),
+    check("password")
+      .not()
+      .isIn(["123", "password123", "god123", "qwerty123", "123qwerty"])
+      .withMessage("Не используйте обычные значения в качестве пароля")
+      .isLength({ min: 6 })
+      .withMessage("Минимальная длина пароля 6 символов")
+      .matches(/\d/)
+      .withMessage("Пароль должен содержать число"),
+  ],
+  async (req, res) => {
+    // базов.логика с обраб.ошб.
+    try {
+      // проверка вход.полей на валидацию
+      const errorsValid = validationResult(req);
+      // е/и проверка не прошла(не пусто) - возвращ.Ответ на front смс ошб.(кастомизируем) + errors.масс.
+      if (errorsValid.isEmpty()) {
+        return res.status(400).json({
+          errors: errorsValid.array(),
+          message: "Некорректые данные при регистрации",
+        });
+      }
 
-    // логика регистр.нов.польз.+ проверки
-    // проверка существ.email по совпад.ключ и значен.
-    const candidate = await User.findOne({ email });
-    // е/и польз.есть - Ответ в смс
-    if (candidate) {
-      return res
-        .status(400)
-        .json({ message: `Такой пользователь уже есть - ${candidate}.` });
+      // получ.данн.с fronta
+      const { email, password } = req.body;
+
+      // ЛОГИКА регистр.нов.польз. + проверки
+      // проверка существ.email по совпад.ключ и значен.
+      const candidate = await User.findOne({ email });
+      // е/и польз.есть - возвращ.Ответ в смс ошб.
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: `Такой пользователь уже есть - ${candidate}.` });
+      }
+
+      // `ждём` hashирование/шифрование пароля ч/з bcryptjs. 1ый пароль, 2ой степень шифр.
+      const hashedPassword = await bcrypt.hash(password, 12);
+      // созд.нов.польз.(пароль совпад.с шифрованым)
+      const user = new User({ email, password: hashedPassword });
+
+      // ждём сохран.от польз.
+      await user.save();
+
+      // promis завершён,польз.созд.в БД. Ответ fronty смс
+      res.status(201).json({ message: `Пользователь с email ${email} создан` });
+    } catch (error) {
+      // общ.отв. на серв.ошб. в json смс
+      res.status(500).json({ message: `Что-то пошло не так - ${error}.` });
     }
-  } catch (error) {
-    // общ.отв. на серв.ошб. в json смс
-    res.status(500).json({ message: `Что-то пошло не так - ${error}.` });
   }
-});
+);
 
 // Логин
 router.post("/login", async (req, res) => {});
